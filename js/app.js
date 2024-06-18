bb7e18a36d133f850e7b055a24d95fb922f17853
NWX1 <99639214+NeoXiong655@users.noreply.github.com>
2024-06-18 09:42:19 -0400

    Add files via upload
    change

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
-let scence,cam,rend;
+let scene,camera,renderer;
 const init=()=>{
-    scence= new Three.Scene();
-    scence.background =new THREE.Color(0x000000);
-    rend =new Three.WebGLRenderer({antialias: true});
-    rend.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(rend.domElement); 
+    scene= new THREE.Scene();
+    scene.background =new THREE.Color(0x000000);
+    renderer =new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement); 
     const aspect =window.innerWidth/window.innerHeight;
-    cam=new THREE.PerspectiveCamera(60,aspect,0.01,5000);
-    cam.positon.set(0.5,0,0);
+    camera=new THREE.PerspectiveCamera(60,aspect,0.01,5000);
+    camera.positon.set(0.5,0,0);
 
     ambientLight = new THREE.AmbientLight(0x404040, 20);
-    scence.add(ambientLight);
+    scene.add(ambientLight);
 
     const loader = new THREE.GLTFLoader();
     loader.load("RB18.gltf",(result) => {
-        scence.add(result.scence);
-        rend.render(scence,cam);
+        scene.add(result.scene);
+        renderer.render(scene,camera);
     });
 }
 
